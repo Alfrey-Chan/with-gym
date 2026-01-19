b6471ec8dae9b3c3a1e103f0470890a626e50c13
Alfrey-Chan <122188063+Alfrey-Chan@users.noreply.github.com>
2026-01-19 13:42:32 -0800

    feat: create banner component

---
+++ src/components/Banner/Banner.tsx
@@ -0,0 +1,41 @@
+import styles from "./Banner.module.css";
+
+function Banner() {
+	return (
+		<div className={styles.banner}>
+			<svg viewBox="0 0 24 24" fill="none">
+				<circle
+					cx="12"
+					cy="12"
+					r="9"
+					stroke="currentColor"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="1.75"
+				/>
+				<rect
+					height="0.01"
+					stroke="currentColor"
+					stroke-linejoin="round"
+					stroke-width="2.5"
+					width="0.01"
+					x="12"
+					y="16"
+				/>
+				<path
+					d="M12 12L12 8"
+					stroke="currentColor"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+				/>
+			</svg>
+			<p className={styles.text}>
+				<strong>お知らせ：　</strong>
+				今週の土曜クラスは設備点検のため第2体育館で行います。
+			</p>
+		</div>
+	);
+}
+
+export default Banner;
