11b1f9d8019ee1d83a00be4553f46dd6df874741
Alfrey-Chan <122188063+Alfrey-Chan@users.noreply.github.com>
2026-01-19 00:57:34 -0800

    feat: add AppShell as app skeleton layout

--- src/components/layout/AppShell/AppShell.tsx
+++ src/components/layout/AppShell/AppShell.tsx
@@ -1,12 +1,21 @@
 import type { ReactNode } from "react";
 import styles from "./AppShell.module.css";
+import AppNav from "./../../../components/AppNav/AppNav";
+import AppHeader from "../../AppHeader/AppHeader";
 
 type AppShellProps = {
 	children: ReactNode;
 };
 
 function AppShell({ children }: AppShellProps) {
-	return <></>;
+	return (
+		<div className={styles.shell}>
+			<AppHeader />
+			<AppNav />
+
+			<main className={styles.main}>{children}</main>
+		</div>
+	);
 }
 
 export default AppShell;
