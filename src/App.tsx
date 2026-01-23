99c924d44c99ec77491c9865953d9281f8397f79
Alfrey-Chan <122188063+Alfrey-Chan@users.noreply.github.com>
2026-01-23 00:25:21 -0800

    feat: add routing in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { Route, Routes } from "react-router-dom";
 import AppShell from "./components/layout/AppShell/AppShell";
 import CalendarPage from "./pages/CalendarPage/CalendarPage";
+import ProgressPage from "./pages/ProgressPage/ProgressPage";
 
 function App() {
 	return (
 		<AppShell>
-			<CalendarPage />
+			<Routes>
+				<Route path="/" element={<CalendarPage />} />
+				<Route path="/calendar" element={<CalendarPage />} />
+				<Route path="/progress" element={<ProgressPage />} />
+			</Routes>
 		</AppShell>
 	);
 }
